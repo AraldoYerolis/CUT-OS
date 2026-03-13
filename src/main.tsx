3338bf9103f7ef46a9c237d3338549e3362ec453
AraldoYerolis <109188011+AraldoYerolis@users.noreply.github.com>
2026-03-13 14:04:32 -0400

    Refactor main.tsx to use BrowserRouter and React.StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/reset.css'
-import './styles/tokens.css'
-import './styles/global.css'
-import { App } from './app/App'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './app/App'
 
-const root = document.getElementById('root')
-if (!root) throw new Error('Root element not found')
-
-createRoot(root).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/CUT-OS">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
